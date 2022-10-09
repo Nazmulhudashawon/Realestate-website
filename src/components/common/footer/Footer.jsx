efee9971d6871150194d4ee8d9593e9a8fc3a8d3
Nazmulhudashawon <shawonnazmulhuda@gmail.com>
2022-10-09 21:26:03 +0600

    final

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
         <div className='container'>
           <div className='box'>
             <div className='logo'>
-              <img src='../images/logo-light.png' alt='' />
+              <h1>MadeUP</h1>
               <h2>Do You Need Help With Anything?</h2>
               <p>Receive updates, hot deals, tutorials, discounts sent straignt in your inbox every month</p>
 
@@ -45,7 +45,7 @@ const Footer = () => {
         </div>
       </footer>
       <div className='legal'>
-        <span>© 2021 RentUP. Designd By GorkCoder.</span>
+        <span>© 2022 MadeUP. Designd By Nazmul Huda </span>
       </div>
     </>
   )
--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -10,8 +10,8 @@ const Header = () => {
     <>
       <header>
         <div className='container flex'>
-          <div className='logo'>
-            <img src='./images/logo.png' alt='' />
+          <div>
+           <h1>MadeUP</h1>
           </div>
           <div className='nav'>
             <ul className={navList ? "small" : "flex"}>
@@ -23,9 +23,7 @@ const Header = () => {
             </ul>
           </div>
           <div className='button flex'>
-            <h4>
-              <span>2</span> My List
-            </h4>
+      
             <button className='btn1'>
               <i className='fa fa-sign-out'></i> Sign In
             </button>
